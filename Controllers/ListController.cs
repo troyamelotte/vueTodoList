4a48ff5c2e1754e8b996b9ebc661da464871865c
troy amelotte <tamelotte@codingdojo.com>
2017-03-18 18:13:57 -0700

    fully removed models (was not needed)

--- Controllers/ListController.cs
+++ Controllers/ListController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using DbConnection;
-using TodoList.Models;
 namespace TodoList.Controllers
 {
     public class ListController : Controller
